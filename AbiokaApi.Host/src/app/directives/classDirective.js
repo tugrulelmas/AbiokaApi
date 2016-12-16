ef5bb8883d5d3450041e84d1031735d3fb34d129
tugrul <tugrulelmas@gmail.com>
2016-12-16 08:48:24 +0300

    added responsive dialog class

---
+++ AbiokaApi.Host/src/app/directives/classDirective.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('abioka')
+      .directive('classGtSm', classGtSm);
+
+    /* @ngInject */
+    function classGtSm($mdMedia) {
+        if ($mdMedia("gt-sm")) {
+            return {
+                restrict: "A",
+                link: function (scope, elem, attrs) {
+                    elem.addClass(attrs.classGtSm);
+                }
+            };
+        }
+    }
+})();
