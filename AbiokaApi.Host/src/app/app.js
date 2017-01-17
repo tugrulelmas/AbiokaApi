d5e28252914c415e7571624a7820cdb5d676b8b7
tugrul <tugrulelmas@gmail.com>
2017-01-17 10:08:48 +0300

    css

--- AbiokaApi.Host/src/app/app.js
+++ AbiokaApi.Host/src/app/app.js
@@ -19,8 +19,8 @@
 
         $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
             if (toState.isPublic === true) {
-                settingsService.setBackImg("../assets/images/login.jpg");
-                //settingsService.setBackImg("https://source.unsplash.com/collection/1052/800x600");
+                //settingsService.setBackImg("../assets/images/login.jpg");
+                settingsService.setBackImg("https://source.unsplash.com/random");
                 return;
             }
 
