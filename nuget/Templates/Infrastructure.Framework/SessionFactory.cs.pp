6a7548bdc289c2e07e3d2eb549f7959d482110c5
tugrul <tugrulelmas@gmail.com>
2017-02-05 00:21:27 +0300

    added SessionFactory.cs.pp

---
+++ nuget/Templates/Infrastructure.Framework/SessionFactory.cs.pp
@@ -0,0 +1,24 @@
+﻿using AbiokaApi.Infrastructure.Common.ApplicationSettings;
+using AbiokaApi.Infrastructure.Common.IoC;
+using AbiokaApi.Repository.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace $rootnamespace$
+{
+    internal class SessionFactory
+    {
+        public static ISessionFactory CreateNhSessionFactory() {
+            var connectionStringRepository = DependencyContainer.Container.Resolve<IConnectionStringRepository>();
+            var sessionFactory = Fluently.Configure()
+          .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionStringRepository.ReadConnectionString("abioka")))
+          .ExposeConfiguration(config => new SchemaUpdate(config).Execute(false, true))
+          .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>()
+                         .Conventions.Add(new UtcConvention()))
+          .BuildSessionFactory();
+            return sessionFactory;
+        }
+    }
+}
