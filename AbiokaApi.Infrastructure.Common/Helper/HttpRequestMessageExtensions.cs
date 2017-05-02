8da0aee3091d8f7846206a705db707db4a846f47
tugrul <tugrulelmas@gmail.com>
2017-05-02 10:42:33 +0300

    added cloud flare ip header check

--- AbiokaApi.Infrastructure.Common/Helper/HttpRequestMessageExtensions.cs
+++ AbiokaApi.Infrastructure.Common/Helper/HttpRequestMessageExtensions.cs
@@ -1,31 +1,44 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
 
 namespace AbiokaApi.Infrastructure.Common.Helper
 {
     public static class HttpRequestMessageExtensions
     {
-        private const string HttpContext = "MS_HttpContext";
-        private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
-        private const string OwinContext = "MS_OwinContext";
+        private const string httpContext = "MS_HttpContext";
+        private const string remoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
+        private const string owinContext = "MS_OwinContext";
+        private const string cloudFlare = "CF-Connecting-IP";
 
         public static string GetClientIpString(this HttpRequestMessage request) {
+            //"CF-Connecting-IP"
+            if (request.Headers.TryGetValues(cloudFlare, out IEnumerable<string> clientIPs)) {
+                if(clientIPs!= null && clientIPs.Count() > 0) {
+                    var clientIp = clientIPs.First();
+                    if (!string.IsNullOrWhiteSpace(clientIp)) {
+                        return clientIp;
+                    }
+                }
+            }
+
             //Web-hosting
-            if (request.Properties.ContainsKey(HttpContext)) {
-                dynamic ctx = request.Properties[HttpContext];
+            if (request.Properties.ContainsKey(httpContext)) {
+                dynamic ctx = request.Properties[httpContext];
                 if (ctx != null) {
                     return ctx.Request.UserHostAddress;
                 }
             }
             //Self-hosting
-            if (request.Properties.ContainsKey(RemoteEndpointMessage)) {
-                dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+            if (request.Properties.ContainsKey(remoteEndpointMessage)) {
+                dynamic remoteEndpoint = request.Properties[remoteEndpointMessage];
                 if (remoteEndpoint != null) {
                     return remoteEndpoint.Address;
                 }
             }
             //Owin-hosting
-            if (request.Properties.ContainsKey(OwinContext)) {
-                dynamic ctx = request.Properties[OwinContext];
+            if (request.Properties.ContainsKey(owinContext)) {
+                dynamic ctx = request.Properties[owinContext];
                 if (ctx != null) {
                     return ctx.Request.RemoteIpAddress;
                 }
