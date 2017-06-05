5676fe6e9ac7a21ff105d0ffbe34afaa9ecb35b1
Tugrul Elmas <tugrulelmas@gmail.com>
2017-06-05 13:38:26 +0300

    added ExceptionLogHandlerTest

---
+++ AbiokaApi.UnitTest/Service/ExceptionLogHandlerTest.cs
@@ -0,0 +1,41 @@
+﻿using AbiokaApi.Infrastructure.Common.ApplicationSettings;
+using AbiokaApi.Infrastructure.Common.Dynamic;
+using AbiokaApi.Infrastructure.Common.Exceptions;
+using AbiokaApi.UnitTest.Service.Mock;
+using Moq;
+using NUnit.Framework;
+
+namespace AbiokaApi.UnitTest.Service
+{
+    [TestFixture]
+    public class ExceptionLogHandlerTest
+    {
+        [Test]
+        public void Do_Log_When_IsExceptionLogEnabled() {
+            var connectionStringRepository = new Mock<IConnectionStringRepository>();
+            connectionStringRepository.Setup(c => c.ReadAppSetting("IsExceptionLogEnabled")).Returns("true");
+            var exceptionLogHandlerMock = ExceptionLogHandlerMock.Create(connectionStringRepository);
+
+            IExceptionContext exceptionContext = new ExceptionContext(null);
+            var exceptionLog = new ExceptionLog();
+            exceptionLogHandlerMock.ExceptionLogResolverMock.Setup(e => e.Resolve(exceptionContext)).Returns(exceptionLog);
+
+            exceptionLogHandlerMock.OnException(exceptionContext);
+
+            exceptionLogHandlerMock.ExceptionLogResolverMock.Verify(e => e.Resolve(exceptionContext), Times.Once());
+            exceptionLogHandlerMock.ExceptionLogRepositoryMock.Verify(e => e.Add(exceptionLog), Times.Once());
+        }
+
+        [Test]
+        public void Do_Not_Log_When_IsExceptionLogDisabled() {
+            var connectionStringRepository = new Mock<IConnectionStringRepository>();
+            connectionStringRepository.Setup(c => c.ReadAppSetting("IsExceptionLogEnabled")).Returns("false");
+            var exceptionLogHandlerMock = ExceptionLogHandlerMock.Create(connectionStringRepository);
+
+            IExceptionContext exceptionContext = new ExceptionContext(null);
+            exceptionLogHandlerMock.OnException(exceptionContext);
+
+            exceptionLogHandlerMock.ExceptionLogResolverMock.Verify(e => e.Resolve(exceptionContext), Times.Never());
+        }
+    }
+}
---
+++ AbiokaApi.UnitTest/Service/Mock/ExceptionLogHandlerMock.cs
@@ -0,0 +1,24 @@
+﻿using AbiokaApi.ApplicationService.Handlers;
+using AbiokaApi.Domain.Repositories;
+using AbiokaApi.Infrastructure.Common.ApplicationSettings;
+using AbiokaApi.Infrastructure.Common.Exceptions;
+using Moq;
+
+namespace AbiokaApi.UnitTest.Service.Mock
+{
+    class ExceptionLogHandlerMock : ExceptionLogHandler
+    {
+        public readonly Mock<IExceptionLogResolver> ExceptionLogResolverMock;
+        public readonly Mock<IExceptionLogRepository> ExceptionLogRepositoryMock;
+        public readonly Mock<IConnectionStringRepository> ConnectionStringRepositoryMock;
+
+        public ExceptionLogHandlerMock(Mock<IExceptionLogResolver> exceptionLogResolver, Mock<IExceptionLogRepository> exceptionLogRepository, Mock<IConnectionStringRepository> connectionStringRepository)
+            : base(exceptionLogResolver.Object, exceptionLogRepository.Object, connectionStringRepository.Object) {
+            ExceptionLogResolverMock = exceptionLogResolver;
+            ExceptionLogRepositoryMock = exceptionLogRepository;
+            ConnectionStringRepositoryMock = connectionStringRepository;
+        }
+
+        public static ExceptionLogHandlerMock Create(Mock<IConnectionStringRepository> connectionStringRepository) => new ExceptionLogHandlerMock(new Mock<IExceptionLogResolver>(), new Mock<IExceptionLogRepository>(), connectionStringRepository);
+    }
+}
