89ede7abb287334e1bc36c75b0a7772420d1f0bb
Tugrul Elmas <tugrulelmas@gmail.com>
2017-06-10 14:22:19 +0300

    fix: normal users see all login logs

--- AbiokaApi.Repository/Bootstrapper.cs
+++ AbiokaApi.Repository/Bootstrapper.cs
@@ -1,6 +1,8 @@
-﻿using AbiokaApi.Infrastructure.Common.Domain;
+﻿using AbiokaApi.Domain;
+using AbiokaApi.Infrastructure.Common.Domain;
 using AbiokaApi.Infrastructure.Common.Dynamic;
 using AbiokaApi.Infrastructure.Common.IoC;
+using AbiokaApi.Repository.Repositories;
 
 namespace AbiokaApi.Repository
 {
@@ -12,6 +14,7 @@ namespace AbiokaApi.Repository
             DependencyContainer.Container
                 .UsingFactoryMethod(SessionFactory.CreateNhSessionFactory, true)
                 .RegisterWithDefaultInterfaces(typeof(IRepository<>), typeof(Repository<>))
+                .Register<IRepository<LoginAttempt>, LoginAttemptRepository>()
                 .Register<IDynamicHandler, NhUnitOfWorkHandler>(LifeStyle.PerWebRequest)
                 .Register<IUnitOfWork, UnitOfWork>(LifeStyle.PerWebRequest, true)
                 .Register<IDisposableUnitOfWork, DisposableUnitOfWork>(LifeStyle.Transient, true);
